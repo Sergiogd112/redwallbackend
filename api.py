ce55668551c59f95cadb349e01717a20d91011ef
Sergiogd112 <sergio.gomezdamas@gmail.com>
2020-08-05 17:35:42 +0200

    Update

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import json
 
 
 def get_top_img_urls(dic=None, sub=None, n=1, period='d'):
-    if(not(sub)):
+    if (not (sub)):
         sub = dic['sub']
         try:
             n = int(dic['n'])
@@ -16,18 +16,38 @@ def get_top_img_urls(dic=None, sub=None, n=1, period='d'):
             pass
     print(sub, n, period)
     submission = get_top_images.get_top_submissions(sub, n, period)
-    data = [x for x in get_top_images.image_urls(submission)]
+    data = [[x, y, z, sub] for (x, y, z) in get_top_images.image_urls(submission)]
     return json.dumps({'response': data})
 
 
 def get_icon_sub(dic=None, sub=None):
     r = praw.Reddit(user_agent='nagracks')
 
-    if(not(sub)):
+    if (not (sub)):
         sub = dic['sub']
     print('get icon:', sub)
     return json.dumps({'url': r.get_subreddit(sub).icon_img})
 
 
+def get_cured_(dic=None, subs=None, n=20, per='d'):
+    if (not (subs)):
+        subs = dic['subs']
+        try:
+            n = dic['n']
+        except:
+            pass
+        try:
+            per = dic['per']
+        except:
+            pass
+    data = []
+
+    for sub in subs:
+        data += json.loads(get_top_img_urls(sub=sub, n=n, period=per))['response']
+    data2 = sorted(data, key=lambda l: l[1])[::-1]
+    return json.dumps(data2)
+
+
 if __name__ == '__main__':
-    print(get_top_img_urls(sub='EarthPorn', n=20, period='d'))
+    (get_top_img_urls(sub='EarthPorn', n=20, period='d'))
+    get_cured_(subs=['EarthPorn', 'Wallpaper', 'MinimalWallpaper','phonewallpapers'])
